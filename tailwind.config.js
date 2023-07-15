4342543c76c6f339ce30c01995e26fd98c8a767e
tobii tobii <00tobi.tech00@gmail.com>
2023-07-15 23:55:13 +0000

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
+        primary: "#000000",
+        secondary: "#dff7f1",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
@@ -20,7 +20,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/herobg-02.png')",
       },
     },
   },
